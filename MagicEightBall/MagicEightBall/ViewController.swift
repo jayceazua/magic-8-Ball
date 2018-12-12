395569567863136c2d9b882881b9d11f984097c3
Jayce Azua <jayce.azua@gmail.com>
2018-12-12 03:56:27 -0800

    learning...

--- MagicEightBall/MagicEightBall/ViewController.swift
+++ MagicEightBall/MagicEightBall/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var shakeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
